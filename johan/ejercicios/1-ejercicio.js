f910ed75d67591b23d4aa522b527a773a8bd416f
Jdelahoz05 <jdelahoz@sol4it.net>
2023-02-27 18:22:35 -0500

    ejercicios refactor

--- johan/ejercicios/1-ejercicio.js
+++ johan/ejercicios/1-ejercicio.js
@@ -1,9 +1,9 @@
 //1- Tienes un array de números entre positivos y negativos, tu reto es retornar un objeto con el número de veces que aparece un número.
 
 const array = [1, 2, 2, 3, 3, 3]
-const counts = {};
 
-function numOccurrences(Input) {
+function numOccurrences(array) {
+const counts = {};
 array.forEach(element => {
     if (counts[element] === undefined) {
         counts[element] = 1;
@@ -14,4 +14,4 @@ array.forEach(element => {
 return console.log(counts);
 } 
 
-numOccurrences(Input);
+numOccurrences(array);
--- johan/ejercicios/3-ejercicio.js
+++ johan/ejercicios/3-ejercicio.js
@@ -1,8 +1,8 @@
 array = ["ana", "santi", "nico", "anastasia"]
 term = "ana"
-let resultado = []
 
 function search(array, term) {
+    let resultado = []
     for (let i = 0; i < array.length; i++) {
         if (array[i].includes(term)) {
             resultado.push(array[i]);
