cb560a077b4615f5590e8eb0facaaad6ae5dfa65
HatakeSunflower <st1809149@upy.edu.mx>
2023-03-01 17:59:24 -0600

    v3

--- Anwar/Ejercicios/ejercicio_2.js
+++ Anwar/Ejercicios/ejercicio_2.js
@@ -1,23 +1,86 @@
-/*
-Ejercicios de SuiteScript: ClientScript
+/**
+ * Anwar Ruiz
+ *@NApiVersion 2.1
+ *@NScriptType ClientScript
+ */
+define([], function () {
+    // A) Cuando el Formulario inicie en modo creación se debe establecer por defecto el cliente
+    function pageInit(context) {
+        var currentRecord = context.currentRecord;
+        var mode = context.mode;
+        if (mode === 'create' && currentRecord.type === 'salesorder') {
+            currentRecord.setValue({
+                fieldId: 'entity',
+                value: 1691,
+            });
+        }
+    }
 
-1- Para la práctica presente se requiere implementar un Script de tipo Cliente Script en el formulario de Orden de Venta y el cual se debe seleccionar el entrypoints adecuado para hacer las siguientes ejecuciones:
+    function lineInit(context) {
+        // E) Establecer 2 por defecto
+        var currentRecord = context.currentRecord;
+        var mode = context.mode;
+        var sublistId = context.sublistId;
+        var line = context.line;
+        if (mode === 'create' && sublistId === 'item') {
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'quantity',
+                value: 2,
+            });
+        }
+        return true;
+    }
 
-Nota: Para esta actividad se requiere Crear un Cliente con anterioridad, este sera usado a lo largo de la actividad.
+    function validateLine(context) {
+        // C) Validar que la cantidad de un artículo no supere 5 unidades
+        var quantity = context.currentRecord.getCurrentSublistValue({
+            sublistId: 'item',
+            fieldId: 'quantity',
+        });
+        if (quantity > 5) {
+            alert('La cantidad no puede ser mayor a 5 unidades.');
+            return false; // Impedir el guardado
+        }
+        return true;
+    }
 
-A) Cuando el Formulario inicie en modo creación se debe establecer por defecto el cliente creado con anterioridad.
+    function saveRecord(context) {
+        // B) Validar que el pedido tenga mínimo 3 líneas 
+        var lineCount = context.currentRecord.getLineCount({ sublistId: 'item' });
+        if (lineCount < 3) {
+            alert('La transacción debe tener como mínimo 3 líneas para guardarse.');
+            return false; // Detener el guardado
+        }
+        return true;
+    }
 
-B) Cuando se guarden los cambios del registro, se debe validar que el pedido contenga mínimo 3 línea de transacción, si no las contiene mostrar en pantalla con un mensaje nativo de NetSuite que diga "la transacción debe tener como minimo 3 Lineas para guardarse" y arrojar este mensaje cada vez que guarde hasta que cumpla la condición antes mencionada.
+    function fieldChanged(context) {
+        // D) Rellenar el campo Descripción con el articulo
+        if (context.fieldId === 'item') {
+            var item = context.currentRecord.getCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'item'
+            });
 
-C) Al editar o introducir la cantidad de un artículo se debe garantizar que el número no pueda superar 5 unidades
+        }
+    }
 
-D) cada vez que cambies o selecciones un artículo rellena el campo Descripción con el texto encontrado en la categoría de costo
+    return {
+        pageInit: pageInit,
+        lineInit: lineInit,
+        validateLine: validateLine,
+        saveRecord: saveRecord,
+        fieldChanged: fieldChanged
+    };
 
-E) Cada vez que se inicie una linea de articulo el número 2 debe estar por default en el campo Cantidad
+});
 
-Fecha de Entrega:
-
-Semillero NetSuite: 01 de Marzo/ 7:00 pm
-
-Api Center: 03 de Marzo/ 7:00 pm
+/* Netsuite References 
+https://tstdrv2720065.app.netsuite.com/app/help/helpcenter.nl?fid=section_4387798404.html#bridgehead_4484779426
+https://tstdrv2720065.app.netsuite.com/app/help/helpcenter.nl?fid=section_4637584890.html
+https://tstdrv2720065.app.netsuite.com/app/help/helpcenter.nl?fid=section_4273153320.html
+*/
+/*Thirdparty References
+https://stackoverflow.com/questions/10729198/what-does-return-false-do
 */
