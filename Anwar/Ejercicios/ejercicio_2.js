bdbb7429cc139d366103ffe08568340f908f7f8f
HatakeSunflower <st1809149@upy.edu.mx>
2023-03-01 11:08:46 -0600

    re

--- Anwar/Ejercicios/ejercicio_2.js
+++ Anwar/Ejercicios/ejercicio_2.js
@@ -1,62 +1,23 @@
 /*
-2 - Tienes un array de objetos que representan órdenes de compra con los siguientes atributos:
+Ejercicios de SuiteScript: ClientScript
 
-customerName: string
-total: number
-delivered: boolean
+1- Para la práctica presente se requiere implementar un Script de tipo Cliente Script en el formulario de Orden de Venta y el cual se debe seleccionar el entrypoints adecuado para hacer las siguientes ejecuciones:
 
-Tu reto es retornar la suma total de todas las órdenes de compra, para solucionarlo crea una función que recibe un parámetro de entrada:
+Nota: Para esta actividad se requiere Crear un Cliente con anterioridad, este sera usado a lo largo de la actividad.
 
-orders: Un array con las órdenes de compra
+A) Cuando el Formulario inicie en modo creación se debe establecer por defecto el cliente creado con anterioridad.
 
-    < code >
-    Input:
-[
-    {
-        customerName: "Nicolas",
-        total: 100,
-        delivered: true,
-    },
-    {
-        customerName: "Zulema",
-        total: 120,
-        delivered: false,
-    },
-    {
-        customerName: "Santiago",
-        total: 20,
-        delivered: false,
-    }
-]
-Ouput: 240
-</ >
+B) Cuando se guarden los cambios del registro, se debe validar que el pedido contenga mínimo 3 línea de transacción, si no las contiene mostrar en pantalla con un mensaje nativo de NetSuite que diga "la transacción debe tener como minimo 3 Lineas para guardarse" y arrojar este mensaje cada vez que guarde hasta que cumpla la condición antes mencionada.
+
+C) Al editar o introducir la cantidad de un artículo se debe garantizar que el número no pueda superar 5 unidades
+
+D) cada vez que cambies o selecciones un artículo rellena el campo Descripción con el texto encontrado en la categoría de costo
+
+E) Cada vez que se inicie una linea de articulo el número 2 debe estar por default en el campo Cantidad
+
+Fecha de Entrega:
+
+Semillero NetSuite: 01 de Marzo/ 7:00 pm
+
+Api Center: 03 de Marzo/ 7:00 pm
 */
-
-const array = [{
-    customerName: "Nicolas",
-    total: 100,
-    delivered: true,
-},
-{
-    customerName: "Zulema",
-    total: 120,
-    delivered: false,
-},
-{
-    customerName: "Santiago",
-    total: 20,
-    delivered: false,
-}
-];
-
-function add(array) {
-    let addition = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        addition += array[i].total;
-    }
-
-    console.log(addition);
-}
-
-add(array);
