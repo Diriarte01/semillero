4a8762050cf988bb2b0b99c028276cac7f6fa32a
catmorx <alismora3@gmail.com>
2023-03-03 23:48:11 -0500

    Ejercicio RESTlet completo

--- Catalina/Ejercicios/rl-ejecicio.js
+++ Catalina/Ejercicios/rl-ejecicio.js
@@ -1,33 +1,33 @@
 /**
  *@NApiVersion 2.1
  *@NScriptType Restlet
- *
+ *@author Catalina R
+ *Fecha 03/03/2023
  */
 define(["N/search", "N/record"], function (search, record) {
 
     function _get(context) {
-        //?Búsqueda
         const response = { code: 400, success: false, data: [], error: [] };
         try {
+            const type = "contact";
+            const filters = [];
+            const contact = context.id;
+
+            /* Busqueda de un solo contacto por id, sino busqueda a todas */
+            if (contact) {
+                filters.push(["internalid", "anyof", contact])
+            }
+
+            const columns = [search.createColumn({ name: "entityid", sort: search.Sort.ASC, label: "Nombre" })];
+            columns.push(search.createColumn({ name: "email", label: "Correo electrónico" }));
+            columns.push(search.createColumn({ name: "phone", label: "Teléfono" }));
+            columns.push(search.createColumn({ name: "company", label: "Empresa" }));
+            columns.push(search.createColumn({ name: "title", label: "Puesto de trabajo" }));
+            columns.push(search.createColumn({ name: "subsidiary", label: "subsidiria" }));
+
             let contactSearchObj = search.create({
-                type: "contact",
-                filters: [],
-                columns:
-                    [
-                        search.createColumn({
-                            name: "entityid",
-                            sort: search.Sort.ASC,
-                            label: "Nombre"
-                        }),
-                        search.createColumn({ name: "email", label: "Correo electrónico" }),
-                        search.createColumn({ name: "phone", label: "Teléfono" }),
-                        search.createColumn({ name: "company", label: "Empresa" }),
-                        search.createColumn({ name: "title", label: "Puesto de trabajo" }),
-                        search.createColumn({ name: "subsidiary", label: "subsidiria" })
-                    ]
+                type: type, filters: filters, columns: columns,
             });
-            let searchResultCount = contactSearchObj.runPaged().count;
-            log.debug("contactSearchObj result count", searchResultCount);
             contactSearchObj.run().each(function (result) {
                 let obj = new Object();
                 obj.internalId = result.id;
@@ -38,7 +38,7 @@ define(["N/search", "N/record"], function (search, record) {
                 obj.title = result.getValue("title");
                 obj.subsidiary = result.getValue("subsidiary");
                 response.data.push(obj);
-                response.code = 201;
+                response.code = 200;
                 response.success = true;
                 return true;
             });
@@ -49,10 +49,12 @@ define(["N/search", "N/record"], function (search, record) {
             response.error.push(e.message);
 
         } finally {
-            return "Has ingresado al API REST de Netsuite" + JSON.stringify(response);
+            return JSON.stringify(response);
         }
     }
 
+
+    /* Creamos con subsidiaria y nombre del contacto por ser campos obligatorios*/
     function _post(context) {
         const response = { code: 400, success: false, data: [], error: [] };
         try {
@@ -61,12 +63,11 @@ define(["N/search", "N/record"], function (search, record) {
                 type: "contact",
                 isDynamic: false,
             })
-            log.debug("data", data);
+
             recordObj.setValue({
                 fieldId: "entityid",
                 value: data.name,
             })
-
             recordObj.setValue({
                 fieldId: "email",
                 value: data.email,
@@ -75,14 +76,26 @@ define(["N/search", "N/record"], function (search, record) {
                 fieldId: "phone",
                 value: data.phone,
             })
+            for (let i = 0; i < data.length; i++) {
+                recordObj.setSublistValue({
+                    sublistId: "company",
+                    fieldId: "company",
+                    value: data.company,
+                    line: i
+                })
+            }
+            recordObj.setValue({
+                fieldId: "title",
+                value: data.title,
+            })
             recordObj.setValue({
                 fieldId: "subsidiary",
                 value: data.subsidiary,
             })
 
             const saveRecord = recordObj.save();
-            response.data.push({ internalId: saveRecord, typeRecord: "contact" })
-            response.code = 200;
+            response.data.push({ internalId: saveRecord, saved: data, typeRecord: "contact" })
+            response.code = 201;
             response.success = true;
         } catch (e) {
             log.error("Hubo un error al create", e.message)
@@ -90,36 +103,50 @@ define(["N/search", "N/record"], function (search, record) {
             response.success = false;
             response.error.push(e.message);
         } finally {
-            return response;
+            return JSON.stringify(response);
         }
     }
+    
 
+    /* Buscamos por el id para cambiar datos o tambien por el nombre sin colocar id */
     function _put(context) {
         const response = { code: 400, success: false, data: [], error: [] };
         try {
             const data = context.data;
-            let name = data.name
-            let contactSearchId = search.create({
-                type: "contact",
-                filters: [["entityid", "is", name]],
-                columns:
-                    [
-                        search.createColumn({
-                            name: "entityid",
-                            label: "Nombre"
-                        }),
-                    ]
+            const contactId = data.internalId
+            const name = data.name;
+            const type = "contact";
+            const filters = [];
+            const columns = [];
+            if (contactId) {
+                filters.push(["internalid", "is", contactId])
+                log.debug("entra en ID", "entra en ID")
+            } else if (name) {
+                filters.push(["entityid", "is", name])
+                columns.push(search.createColumn({ name: "entityid", label: "Nombre" }))
+                log.debug("entra en Nombre", "entra en Nombre")
+            }
+            let contactSearch = search.create({
+                type: type,
+                filters: filters,
+                columns: columns
             })
-            
-            contactSearchId.run().each(function (result) {
+            contactSearch.run().each(function (result) {
                 let obj = new Object();
                 obj.internalId = result.id;
-
                 let recordObj = record.load({
                     type: "contact",
                     id: obj.internalId,
                     isDynamic: false,
                 })
+                for (let i = 0; i < data.length; i++) {
+                    recordObj.setSublistValue({
+                        sublistId: "company",
+                        fieldId: "company",
+                        value: data.company,
+                        line: i
+                    })
+                }
                 recordObj.setValue({
                     fieldId: "entityid",
                     value: data.name,
@@ -132,68 +159,54 @@ define(["N/search", "N/record"], function (search, record) {
                     fieldId: "phone",
                     value: data.phone,
                 })
+
+                recordObj.setValue({
+                    fieldId: "title",
+                    value: data.title,
+                })
                 recordObj.setValue({
                     fieldId: "subsidiary",
                     value: data.subsidiary,
                 })
-                
+
                 const saveRecord = recordObj.save();
-                
-                response.data.push({ internalId: saveRecord, typeRecord: "contact" })
+                // log.debug("data saved", data)
+                response.data.push({ internalId: saveRecord, changed: data, typeRecord: "contact" })
                 response.code = 200;
                 response.success = true;
                 return true;
             });
-            
-
         } catch (e) {
-            log.error("search result id error", e.message)
+            log.error("search name error", e.message)
             response.code = 500;
             response.success = false;
-            response.error = (e.message);
+            response.error = (e.message + " O los campos obligatorios subsidiaria y nombre deberian tener un valor");
         } finally {
-            return "Has ingresado al API REST de Netsuite" + JSON.stringify(response);
+            return JSON.stringify(response);
         }
     }
-
+    /* Escribimos en los params de postman con el id &id=2001 este numbero será el contacto a eliminar
+    y en el body dejamos unos brackets vacios  */
     function _delete(context) {
         const response = { code: 400, success: false, data: [], error: [] };
         try {
+            log.debug("context, con")
+            let contactId = context.id;
 
-            log.debug("context", context);
-            // const data = context.data;
-            // let name = data.name
-            // let contactSearchId = search.create({
-            //     type: "contact",
-            //     filters: [["entityid", "is", name]],
-            //     columns:
-            //         [
-            //             search.createColumn({
-            //                 name: "entityid",
-            //                 label: "Nombre"
-            //             }),
-            //         ]
-            // })
-            // contactSearchId.run().each(function (result) {
-            //     let obj = new Object();
-            //     obj.internalId = result.id;
-
-            //     let recordObj = record.delete({
-            //         type: "contact",
-            //         id: obj.internalId,
-            //         isDynamic: false,
-            //     })
-            //     const saveRecord = recordObj.save();
-            //     response.data.push({ internalId: saveRecord, typeRecord: "contact" })
-            //     response.code = 204;
-            //     response.success = true;
-            // });
+            record.delete({
+                type: "contact",
+                id: contactId
+            });
+            response.data.push({ status: "El contacto ha sido eliminado exitosamente", typeRecord: "contact" })
+            response.code = 204;
+            response.success = true;
         } catch (error) {
             log.debug("Error deleting contact", error.message);
             response.code = 500;
             response.success = false;
-        }finally{
-            return response;
+            response.error = (error.message)
+        } finally {
+            return JSON.stringify(response);
         }
     }
 
